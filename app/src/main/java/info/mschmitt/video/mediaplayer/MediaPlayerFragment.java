6fcd5e372aafba2e78d8be60bdce565829251726
Matthias Schmitt <github@mschmitt.info>
2016-07-14 22:58:04 +0200

    Use retain state framework

--- app/src/main/java/info/mschmitt/video/mediaplayer/MediaPlayerFragment.java
+++ app/src/main/java/info/mschmitt/video/mediaplayer/MediaPlayerFragment.java
@@ -10,16 +10,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.HashMap;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
 import info.mschmitt.video.BR;
+import info.mschmitt.video.commons.DataBindingUtil2;
 import info.mschmitt.video.commons.Factory;
 import info.mschmitt.video.commons.NavigationBarUtils;
 import info.mschmitt.video.databinding.MediaPlayerViewBinding;
+import me.tatarka.retainstate.RetainState;
+import me.tatarka.retainstate.fragment.RetainStateFragment;
 import rx.Completable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -28,13 +29,10 @@ import rx.android.schedulers.AndroidSchedulers;
  * @author Matthias Schmitt
  */
 
-public class MediaPlayerFragment extends Fragment {
-    // Makeshift retained instance state
-    private static HashMap<UUID, MediaPlayerViewModel> viewModelMap = new HashMap<>();
+public class MediaPlayerFragment extends Fragment implements RetainState.Provider {
     @Inject
     public Factory<MediaPlayerViewModel> viewModelFactory;
     public ObservableBoolean systemUIVisible = new ObservableBoolean(true);
-    private UUID id = UUID.randomUUID();
     private MediaPlayerViewModel viewModel;
     private Subscription showSystemUISubscription;
     private Observable.OnPropertyChangedCallback onPropertyChangedCallback =
@@ -47,6 +45,7 @@ public class MediaPlayerFragment extends Fragment {
                     }
                 }
             };
+    private RetainState retainState;
 
     public static MediaPlayerFragment newInstance() {
         return new MediaPlayerFragment();
@@ -98,31 +97,9 @@ public class MediaPlayerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        id = savedInstanceState == null ? UUID.randomUUID() :
-                UUID.fromString(savedInstanceState.getString("ID"));
-        viewModel = viewModelMap.get(id);
-        if (viewModel == null) {
-            viewModel = viewModelFactory.create();
-            viewModelMap.put(id, viewModel);
-        }
         MediaPlayerViewBinding binding = MediaPlayerViewBinding.inflate(inflater, container, false);
-        binding.setViewModel(viewModel);
         binding.setFragment(this);
-        viewModel.addOnPropertyChangedCallback(onPropertyChangedCallback);
-        updateBoundValues(binding, BR._all);
-
-        View decorView = getActivity().getWindow().getDecorView();
-        decorView.setOnSystemUiVisibilityChangeListener(
-                visibility -> viewModel.onSystemFullscreenChanged(
-                        (visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) != 0));
-
         return binding.getRoot();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        outState.putString("ID", id.toString());
-        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -138,14 +115,31 @@ public class MediaPlayerFragment extends Fragment {
     }
 
     @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        viewModelMap.clear();
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        retainState = RetainStateFragment.from(this);
+        viewModel = retainState.retain(0, () -> viewModelFactory.create());
+        viewModel.addOnPropertyChangedCallback(onPropertyChangedCallback);
+        MediaPlayerViewBinding binding = DataBindingUtil2.getBinding(getView());
+        binding.setViewModel(viewModel);
+        updateBoundValues(binding, BR._all);
+        View decorView = getActivity().getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(
+                visibility -> viewModel.onSystemFullscreenChanged(
+                        (visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) != 0));
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Override
     public void onDestroyView() {
         viewModel.removeOnPropertyChangedCallback(onPropertyChangedCallback);
         super.onDestroyView();
+    }
+
+    @Override
+    public RetainState getRetainState() {
+        if (retainState == null) {
+            throw new IllegalStateException("RetainState has not yet been initialized");
+        }
+        return retainState;
     }
 }
